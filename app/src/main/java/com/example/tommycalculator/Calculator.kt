32f07d928e9a20e1614d4bee3dabb4475cd5f5dc
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-11-11 03:47:56 +0700

    Wip

---
+++ app/src/main/java/com/example/tommycalculator/Calculator.kt
@@ -0,0 +1,15 @@
+package com.example.tommycalculator
+
+import android.view.View
+
+interface Calculator {
+
+    fun onDigit(view: View)
+    fun onOperator(view: View)
+    fun onClear(view: View)
+    fun onEqual(view: View)
+    fun onBracketStrat(view: View)
+    fun onvBracketEnd(view: View)
+    fun onDecimal(view: View)
+    fun onDelete(view: View)
+}
--- app/src/main/java/com/example/tommycalculator/MainActivity.kt
+++ app/src/main/java/com/example/tommycalculator/MainActivity.kt
@@ -2,16 +2,64 @@ package com.example.tommycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Calculator {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    var text_result = findViewById<TextView>(R.id.text_result)
+    override fun onDigit(view: View) {
+        if (text_result.text.toString().trim() == "0") text_result.text = ""
+        text_result.append((view as TextView).text.toString())
     }
 
-    fun appendOnExpresentation(string: String, canClear : Boolean){
-        if(canClear){
-            tvResult.text=""
+    override fun onOperator(view: View) {
+        text_result.append("${(view as TextView).text}")
+    }
+
+    override fun onClear(view: View) {
+        text_result.text = "0"
+    }
+
+    override fun onEqual(view: View) {
+        try {
+            val expression = ExpressionBuilder(text_result.text.toString()).build()
+            val result = expression.evaluate()
+            text_result.text = (if (result % 1 > 0) result else
+                result.toInt()).toString()
+        }catch (e: Exception){
+            e.printStackTrace()
         }
     }
+
+    override fun onBracketStrat(view: View) {
+        text_result.append("(")
+    }
+
+    override fun onvBracketEnd(view: View) {
+        text_result.append(")")
+    }
+
+    override fun onDecimal(view: View) {
+        text_result.append(".")
+    }
+
+    override fun onDelete(view: View) {
+        val resultAfterDel :String = text_result.text.toString()
+        if (resultAfterDel.isNotEmpty()){
+            text_result.text = resultAfterDel.dropLast(n = 1 )
+        }else{
+            text_result.text="0"
+        }
+    }
+
+
 }
